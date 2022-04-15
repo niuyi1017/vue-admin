0ceff3b1abb54ffc6dfaf8434d07c2db1f8277bb
niuyi1017 <1096196462@qq.com>
2022-04-15 15:40:04 +0800

    style: format

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="wrapper">
-
+  <div class="login-wrapper">
+    <div class="form-wrapper">
+      <h2 class="form-title"></h2>
+      <el-form label-width="120px">
+        <el-form-item label="Activity name">
+          <div class="svg-content">
+            <el-icon><avatar /></el-icon>
+          </div>
+          <el-input />
+        </el-form-item>
+        <el-form-item label="Activity name">
+          <el-input />
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import { Avatar } from '@element-plus/icons-vue'
+// export default {
+//   //  const form = ref({
+//   //    name: ""
+//   //  })
+// }
 </script>
 
-<style>
-
-</style>
+<style lang="stylus" scoped></style>
